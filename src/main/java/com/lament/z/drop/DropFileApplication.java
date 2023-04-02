1b16bda83f129a377f4f4dd53ea1935308dea00a
Lament <lament.wy@gmail.com>
2023-04-02 16:27:20 +0800

    init from start.spirng.io

---
+++ src/main/java/com/lament/z/drop/DropFileApplication.java
@@ -0,0 +1,13 @@
+package com.lament.z.drop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DropFileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lament/z/drop/DropFileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lament.z.drop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DropFileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
